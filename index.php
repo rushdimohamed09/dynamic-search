9d4bf75c5e6e14df9c0ffb5a0f784b158415e0cf
Rushdi Mohamed <rushdimohamed12@gmail.com>
2020-04-16 13:01:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 include ('functions.php');
 
-$table_name='users';
-$primary_key_column_name='user_id';
-$search_criteria=array('fname'=>'Rushdi', 'lname'=>'Mohamed');
+$table_name='users';//change this 
+$primary_key_column_name='user_id'; // change this 
+$search_criteria=array('fname'=>'Rushdi', 'lname'=>'Mohamed'); // change this 
 
 $data=array('table_name'=>$table_name, 'primary_key_column_name'=>$primary_key_column_name, 'search_criteria'=>$search_criteria );
 $res=json_encode($data);
